f1593ca47cc73c456dbb3f45643d8829218d6e00
guoyunxia <yunxiacomeon@163.com>
2019-11-12 16:34:11 +0800

    完整

---
+++ hello-spring-cloud-admin/src/main/java/com/newpwr/hello/spring/cloud/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.newpwr.hello.spring.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableAdminServer
+@EnableEurekaClient
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
